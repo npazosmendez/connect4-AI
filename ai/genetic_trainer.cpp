be0c9c9931ca6d0d5108ef3e477cd0fbab22e958
pab <pablo.l.balbi@gmail.com>
2017-10-28 23:50:42 -0300

    cross-over partially done

--- ai/genetic_trainer.cpp
+++ ai/genetic_trainer.cpp
@@ -1,4 +1,5 @@
 #include "genetic_trainer.hpp"
+#include <string.h>
 
 
 /*
@@ -25,6 +26,27 @@ pesos gen_trainer::train(){
 
 
 pesos gen_trainer::crossover(pesos p1, pesos p2){
+    uint cross_section = rand() % PESOS_COUNT + 1;
+    pesos p_son = pesos();
+    // inicio el vector de altura del hijo de p1 y p2
+    p_son.alturas = vector<float> (p1.alturas.size());
+    // casteo p1, p2 y p_son a float* para hacer el crossover mas facil
+    float* son_values = (float*) &p_son;
+    float* p1_values = (float*) &p1;
+    float* p2_values = (float*) &p2;
+    // copy first 'cross_section' float parameters
+    memcpy((void*) son_values, (void*) p1_values, sizeof(float)*cross_section);
+    // increment pointer in corresponding qty
+    son_values+=cross_section;
+    p2_values+=cross_section;
+    if (cross_section < PESOS_COUNT) {
+        // el crossover involucra parte del genonma de p2
+        // PESOS_COUNT-cross_section >= 1
+        memcpy((void*) son_values, (void*) p2_values, sizeof(float)*(PESOS_COUNT-cross_section));
+    } // up to this point, p1 and p2 float parameteres have been crossed over
+    // crossover array of heights
+    uint heights_cross_section = rand() % p1.alturas.size() + 1;
+    // TODO: crossover alturas
 
 }
 

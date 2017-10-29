d4548714f0cc2ff60cfedfd9bdcb388b4b944eb8
npazosmendez <npazosmendez@gmail.com>
2017-10-28 22:20:47 -0300

    training.cpp, con una función que cuenta las victorias de un color del log

---
+++ ai/training.cpp
@@ -0,0 +1,31 @@
+
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+// TODO: esto de abajo es bastante rígido, porque depende del 'pwd'
+#define BLUE_LOG_PATH "../log/player_communicator_azul.log"
+#define RED_LOG_PATH "../log/player_communicator_rojo.log"
+
+int contar_victorias(const char* color){
+    /* 'color' debe ser "azul" o "rojo" */
+    ifstream player_log;
+    if (strcmp(color,"azul") == 0) {
+        player_log.open(BLUE_LOG_PATH);
+    }else if(strcmp(color, "rojo") == 0){
+        player_log.open(RED_LOG_PATH);
+    }else{
+        cerr << "Color inválido" << endl;
+        exit(1);
+    }
+    string palabra;
+    int res = 0;
+    while (player_log >> palabra) {
+        if (palabra == "ganaste")
+            res++;
+    }
+    return res;
+}

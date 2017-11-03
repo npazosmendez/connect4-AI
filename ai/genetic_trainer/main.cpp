e0c18b166a1fea73e195cfada0df7185f13de72a
pab <pablo@corre.com>
2017-11-03 13:46:05 -1000

    agregando main para test genetic

---
+++ ai/genetic_trainer/main.cpp
@@ -0,0 +1,13 @@
+#include "genetic_trainer.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    gen_trainer gt = gen_trainer(4, 4, 3, 100, 0.05);
+
+    return 0;
+}
+
+

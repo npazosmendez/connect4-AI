7a2aaf0621d849de00e04a1782dad833a2463566
Juan Lanuza <juan.lanuza3@gmail.com>
2017-11-03 01:01:57 -0300

    agrego funciones para poder entrenar

--- ai/training.cpp
+++ ai/training.cpp
@@ -1,7 +1,11 @@
 #include <string.h>
 #include <iostream>
+#include <stdio.h>
 #include <fstream>
 #include <stdlib.h>
+#include <cstdlib>
+
+#include "golosa.hpp"
 
 using namespace std;
 
@@ -27,4 +31,36 @@ int contar_victorias(const char* color){
             res++;
     }
     return res;
+}
+
+string to_argv(vector<float> pesos){
+    string s;
+    for (uint i = 0; i < pesos.size(); i++) {
+        s += std::to_string(pesos[i]);
+        if (i < pesos.size()-1) s += " ";
+    }
+    return s;
+}
+
+
+int play_with_golosa(uint N, uint M, uint C, uint P, vector<float> pesos, uint games, string rival_exec, bool me_first, string root_loc) {
+    /*
+    PARAMETROS:
+    - N, M, C, P:   Descripcion del tablero
+    - pesos:        Los parametros que se le pasa a la funcion golosa
+    - games:        Cantidad de partidos a Jugar
+    - rival_exec:   Comando para ejecutar el codigo del oponente
+    - me_first:     Indica si el goloso juega primero
+    */
+
+    // python2 c_linea.py —blue_player ai/golosa -1 1 1 1 1 1 1 0 1 2 3 4 5 6 7 —red_player ai/minimax_ab -1 1 1 1 1 1 1 0 1 2 3 4 5 6 7 —first rojo —columns 4 —rows 4 —c 3
+    assert(pesos.size() == golosa::cuantos_parametros(N,M,C));
+    char command[1000];
+    string color_first = me_first ? "rojo" : "azul";
+    string params = to_argv(pesos);
+    sprintf (command, "python %sc_linea.py --blue_player %s --red_player %sgolosa %s --first %s --iterations %d --columns %d --rows %d --c %d --p %d",
+             root_loc.c_str(), rival_exec.c_str(), root_loc.c_str(), params.c_str(), color_first.c_str(), games, N, M, C, P);
+    cout << command << endl;
+    system(command);
+    return contar_victorias("rojo");
 }
---
+++ tests/test_training.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "../ai/training.cpp"
+#include "../ai/golosa.hpp"
+using namespace std;
+
+TEST(training_test, to_argv) {
+    std::vector<float> v(5, 0);
+    for(uint i=0; i<v.size(); i++){
+        v[i] = i;
+    }
+    EXPECT_EQ(to_argv(v), "0.000000 1.000000 2.000000 3.000000 4.000000");
+}
+
+// TEST(training_test, play_with_golosa) {
+//     uint N = 4;
+//     uint M = 4;
+//     uint C = 3;
+//     std::vector<float> pesos(golosa::cuantos_parametros(N,M,C), 1);
+//     play_with_golosa(N, M, C, 20, pesos, 10, "../minimax", true, "../");
+//     play_with_golosa(N, M, C, 20, pesos, 10, "../minimax", false, "../");
+//     EXPECT_GT(contar_victorias("azul"), 0);
+//     EXPECT_GT(contar_victorias("rojo"), 0);
+// }

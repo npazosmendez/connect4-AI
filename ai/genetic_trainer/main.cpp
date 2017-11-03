c95c778c800877a2d2a9563de79730118c908008
pab <pablo.l.balbi@gmail.com>
2017-11-03 17:06:54 -0300

    ahora todo los binarios van a bin/

--- ai/genetic_trainer/main.cpp
+++ ai/genetic_trainer/main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
+    std::cout << "Creando entrenador..." << std::endl;
     gen_trainer gt = gen_trainer(4, 4, 3, 100, 0.05);
+    std::cout << "Entrenando..." << std::endl;
+    auto pesos_posta = gt.train(10);
+    std::cout << "los pesos tienen largo " << pesos_posta.size() << std::endl;
+
 
     return 0;
 }

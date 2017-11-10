a3615c9c9863ca9d791b4fbe3dcb0420e30685ac
Nicolás Pazos <npazosmendez@gmail.com>
2017-11-10 16:11:12 -0300

    genetic

--- ai/genetic_trainer/main.cpp
+++ ai/genetic_trainer/main.cpp
@@ -1,6 +1,6 @@
 #include "genetic_trainer.hpp"
 #include <iostream>
-#include <iomanip> 
+#include <iomanip>
 
 using namespace std;
 
@@ -21,17 +21,15 @@ int main(int argc, char const *argv[]) {
     int pop = atoi(argv[6]);
     float p_mutation = stof(argv[7]);
 
-    std::cout << "Creando entrenador..." << std::endl;
+    // std::cout << "Creando entrenador..." << std::endl;
     gen_trainer gt = gen_trainer(n, m, c, p, gen_limit, 0.006,pop);
     gt.setMutationRate(p_mutation);
-    std::cout << "Entrenando..." << std::endl;
+    // std::cout << "Entrenando..." << std::endl;
     //auto pesos_posta = gt.train(pop);
-    auto pesos_posta = gt.train_random_fitness();
-    std::cout << "imprimo parametros alcanzados: \n";
+    auto pesos_posta = gt.train();
+    // std::cout << "imprimo parametros alcanzados: \n";
     for (uint i = 0; i < pesos_posta.size(); i++) {
         std::cout << std::setprecision(9) << pesos_posta[i] << " ";
     }cout << "\n";
     return 0;
 }
-
-
